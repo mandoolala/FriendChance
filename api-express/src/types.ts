7f0fe9af2e3c56201850ccdbccc0d8c935c5032d
Yejun Kim <no1expman@gmail.com>
2019-11-29 21:43:43 +0900

    Add detailed types for each state

--- api-express/src/types.ts
+++ api-express/src/types.ts
@@ -25,11 +25,42 @@ export interface PromissoryNoteRecord extends PromissoryNoteContent {
   paybackDate: string;
   borrowerId: UserId;
   lenderId: UserId;
-  state: 'draft' | 'requestedConfirm' | 'pendingActivation' | 'activated' | 'ended';
 }
 
-export interface PromissoryNoteResponse extends PromissoryNoteContent {
+export type PromissoryNoteResponse = 
+  PromissoryNoteDraftContent 
+  | PromissoryNoteRequestedConfirmContent 
+  | PromissoryNotePendingActivationContent 
+  | PromissoryNoteEndedContent 
+  | PromissoryNoteActivatedContent;
+
+export interface PromissoryNoteDraftContent extends PromissoryNoteContent {
+  state: 'draft';
+  borrower: User;
+}
+
+export interface PromissoryNoteRequestedConfirmContent extends PromissoryNoteContent {
+  state: 'requestedConfirm';
+  borrower: User;
+}
+
+
+export interface PromissoryNotePendingActivationContent extends PromissoryNoteContent {
+  state: 'pendingActivation';
   lender: User;
   borrower: User;
 }
 
+
+export interface PromissoryNoteEndedContent extends PromissoryNoteContent {
+  state: 'ended';
+  lender: User;
+  borrower: User;
+}
+
+
+export interface PromissoryNoteActivatedContent extends PromissoryNoteContent {
+  state: 'activated';
+  lender: User;
+  borrower: User;
+}
