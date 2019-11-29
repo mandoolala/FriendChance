91bb7fb055ebfafa99a1e3fa949276eada110c03
Yejun Kim <no1expman@gmail.com>
2019-11-29 17:38:46 +0900

    Make it work on browser

--- api-express/src/index.ts
+++ api-express/src/index.ts
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors');
 
 const app = express()
 
@@ -10,12 +11,20 @@ const userById: Partial<{ [key: string]: User }> = {
   }
 }
 
+const nextNoteId = 1;
+const notes: PromissoryNote[] = [];
+
+const findNoteById = (id: string) => {
+  return notes.find(note => note.id === id);
+}
+
 interface User {
   id: string;
   name: string;
 }
 
 interface PromissoryNote {
+  id: string;
   amount: number;
   purpose: string;
   createdAt: string;
@@ -26,6 +35,7 @@ interface PromissoryNote {
   state: 'draft' | 'requestedConfirm' | 'pendingActivation' | 'activated' | 'ended';
 }
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use((req, res, next) => {
   const id = req.headers.authorization;
@@ -40,16 +50,21 @@ app.use((req, res, next) => {
 
 app.get('/user', (req, res) => {
   res.json(req.user)
+});
+
+app.get('/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.get('/notes/:id', (req, res) => {
+  const note = findNoteById(req.params.id);
+  if (note) {
+    res.status(200).json(note);
+  } else {
+    res.status(404).send('Not Found');
+  }
 })
 
-app.get('/test', (req, res) => {
-  res.status(200).send('ok')
-})
-
-app.get('/test/:id', (req, res) => {
-  res.status(200).send(req.params.id)
-})
-
-app.listen(3000, () => {
-  console.log('Server started at port 3000')
+app.listen(8080, () => {
+  console.log('Server started at port 8080')
 })
